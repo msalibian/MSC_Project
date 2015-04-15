affd7b0aac92ce7c7d9f187a921fe78b15985806
kenlau177 <ken_lau177@hotmail.com>
2015-04-14 21:54:29 -0700

    made some minor improvements to documentations in the simulation scripts.

--- code/Modulation/main_simulation_all.m
+++ code/Modulation/main_simulation_all.m
@@ -1,23 +1,40 @@
 
+% Main script that simulates the six modulation formats: OOK, BPSK, OQPSK, 
+%		BFSK-A, BFSK-B, and BFSK-R2.
+
 clear
 
-% path containing the scripts to run simulations
+% Path to reference the scripts that run simulations.
 addpath('C:/Users/Ken/Documents/GitHub/MSC_Project/code/Modulation')
 
-% data output directory for modulation data
+% Data output directory for modulation data.
 out_dir = 'C:/Users/Ken/Documents/GitHub/MSC_Project/data/Modulation';
 
-% observation window size
+% Observation window size.
 N = 512;
-% number of samples per modulation and snrdb
+
+% Number of observations per modulation and SNR.
+% Change variable P to simulate for either training, validation, or testing data.
 %P = 50; % for train data
 %P = 60; % for validation data 
 P = 200; % for test data
-% signal-to-noise ratio
-snrdB_vec = -25:1:13;
-% indicate number of columns in total
-nX = 140;
 
+% Choose a range of signal-to-noise ratio.
+snrdB_vec = -25:1:13;
+
+% Indicate number of columns in total:
+%		This variable helps index the columns when inserting values in the matrix 
+%			containing the data.
+%		A breakdown of the number of columns that amounts to 138:
+%			Column 1: SNR
+%			Columns 2-137: Feature variables
+%			Column 138: Modulation Type (encoded as 1 to 6 <--> OOK, BPSK, ... BFSK-R2).
+nX = 138;
+
+% The following six lines of code call each of the 
+
+% Individually call each function that sets up the parameters 
+%		to simulate each modulation format.
 ook_config_wrapper(out_dir, N, P, snrdB_vec, nX);
 bpsk_config_wrapper(out_dir, N, P, snrdB_vec, nX);
 oqpsk_config_wrapper(out_dir, N, P, snrdB_vec, nX);
--- code/Modulation/ook_config_wrapper.m
+++ code/Modulation/ook_config_wrapper.m
@@ -1,5 +1,20 @@
-%so far we only need to vary the following input parameters
-
+% Simulation of OOK modulation format including pre-processing of 
+%		received signal and feature extraction. The data that is simulated 
+%		is described as follows (see nX description under Parameters):
+%		Column 1 : SNR
+%		Column 2-(nX-1) : Feature variables
+%		Column nX : Modulation format (OOK encoded as 1).
+%	The simulated data is saved to the output directory specified by 
+%		out_dir as described below.
+%
+% Parameters
+% ----------
+% out_dir : output directory on local computer.
+% N : observation window size.
+% P : number of observations per modulation and SNR.
+% snrdB_vec : array of SNRs (integers) eg. -25, -24,....,13
+% nX : number of columns in total.
+%
 function ook_config_wrapper(out_dir, N, P, snrdB_vec, nX)
 	
 	fs = 6.25e6; %sampling frequency
@@ -7,34 +22,35 @@ function ook_config_wrapper(out_dir, N, P, snrdB_vec, nX)
 	sps = round(fs/Rs); %samples per symbol
 	fif = 1e6; %low intermediate frequency
 
-	k1 = 2:8; k2 = 26:28;
+	k1 = 2:8; k2 = 26:28; %frequency indexes
 	B1 = 36; B2 = 18; %params for feature_extract for m3 and m4
+		
 	% normalized signal power
-    % normalization is necessary to retain unbiased features
-    %  in particular statistical cumulants
-    sigpow = .5; 
+  % normalization is necessary to retain unbiased features
+  %  in particular statistical cumulants
+  sigpow = .5; 
 	norm_factor = 2; %normalizing factor
 
-    % noise sd parameter as a function of snr
+  % Array of parameters of sigmas corresponding to SNRs
 	sd_vec = sqrt(sigpow./(10.^(snrdB_vec/10)));
-	% just a matrix with all zeros to store data later
-    X_all = zeros(P*length(snrdB_vec), nX);
-	% just an index to indicate the next row of X_all
-    k = 1; 
+	% A matrix with all zeros to store data later.
+  X_all = zeros(P*length(snrdB_vec), nX);
+	% An index to indicate the next row of X_all.
+  k = 1; 
 
-    % for each snr simulate P samples
+  % for each snr simulate P samples
 	for s = 1:length(snrdB_vec)
 		sd = sd_vec(s);
 		snrdB = snrdB_vec(s);
 		for p = 1:P
 			% essentially we doubled the obs window size as input 
-            %  however, in the next line of code, we keep only the 
-            %  second half of the signal
-            [xn_tmp, temp, nn_tmp] = ook_modulate(ceil(2*N/sps), fs, sps, fif, sd, norm_factor);
-            % real part of received signal
-            xn = xn_tmp(N:2*N-1);
+      %  however, in the next line of code, we keep only the 
+      %  second half of the signal
+      [xn_tmp, temp, nn_tmp] = ook_modulate(ceil(2*N/sps), fs, sps, fif, sd, norm_factor);
+      % real part of received signal
+      xn = xn_tmp(N:2*N-1);
 			% noise
-            nn = nn_tmp(N:2*N-1);
+      nn = nn_tmp(N:2*N-1);
 			% complex part of received signal
 			feat_vals = feature_extract(xn, k1, k2, B1, B2, nn);
 			X_all(k,:) = [snrdB feat_vals 1];
